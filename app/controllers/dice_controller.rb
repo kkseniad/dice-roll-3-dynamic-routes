338223c5d8bd909aad3c4df5deb58fee77a22f90
Kseniia Kormalova <kormalovak@gmail.com>
2025-09-16 17:29:12 +0000

    refactored

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -3,50 +3,6 @@ class DiceController < ApplicationController
     render({ :template => "dice_templates/homepage" })
   end
 
-  def roll_two_six
-    @rolls = []
-
-    2.times do
-      dice = rand(1..6)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_two_six" })
-  end
-
-  def roll_two_ten
-    @rolls = []
-
-    2.times do
-      dice = rand(1..10)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_two_ten" })
-  end
-
-  def roll_one_twenty
-    @rolls = []
-
-    1.times do
-      dice = rand(1..20)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_one_twenty" })
-  end
-
-  def roll_five_four
-    @rolls = []
-
-    5.times do
-      dice = rand(1..4)
-      @rolls.push(dice)
-    end
-
-    render({ :template => "dice_templates/result_five_four" })
-  end
-
   def roll_flexible
     @num_dice = params.fetch("number_of_dice").to_i
     @sides = params.fetch("how_many_sides").to_i
